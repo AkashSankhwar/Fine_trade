477ca6bb7a099f999be77a749599e1ee75b0d29b
AkashSankhwar <akashsankhwar70@gmail.com>
2026-06-21 12:33:07 +0530

    award section

--- src/landing_page/home/hero.jsx
+++ src/landing_page/home/hero.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+
 function Hero() {
     return ( 
-        <h1>Hero</h1>
+        <div className='container p-5 mb-5'>
+            <div className="row text-center">
+                <img src="/media/images/homeHero.png" alt='Hero image 'className='mb-5'/>
+                <h1 className='mt-5'>Invest in everything</h1>
+                <p>Online plateform in stocks,derivatives,mutual funds,and more</p>
+                <button className='p-2 bg-primary rounded-3 text-white fs-6' style={{width:"30%",margin:"0 auto"}}>Signup now</button>
+            </div>
+
+             
+        </div>
      );
 }
 
