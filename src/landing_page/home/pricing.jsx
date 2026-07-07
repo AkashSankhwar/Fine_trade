4c47a27deadfb277c2bece46416603bfe608c549
AkashSankhwar <akashsankhwar70@gmail.com>
2026-07-07 16:24:47 +0530

    home last page three section

--- src/landing_page/home/pricing.jsx
+++ src/landing_page/home/pricing.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 function Pricing() {
     return ( 
-        <h1>Pricing</h1>
+        <div className='container'>
+            <div className="row">
+               <div className="col-4">
+                <h1 className='mb-3'>unbeatable price</h1>
+                <p> We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges</p>
+                <a href='' >See Pricing <i class="fa-solid fa-arrow-right-long"></i></a>
+                </div> 
+               <div className="col-2"></div> 
+               <div className="col-6">
+                <div className="row" >
+                    <div className="col p-2 border">
+                        <h1> ₹0</h1>
+                        <p>Free delivery and direct mutual funds</p>
+                    </div>
+                    <div className=" col p-2 border">
+                        <h1> ₹20</h1>
+                        <p>Intraday F&O</p>
+                    </div>
+                </div>
+                </div> 
+            </div>
+
+        </div>
      );
 }
 
