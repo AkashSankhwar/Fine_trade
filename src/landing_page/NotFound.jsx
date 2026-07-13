4dc7cefcd76e76f69989c8398c7f0c8bacbaf068
AkashSankhwar <akashsankhwar70@gmail.com>
2026-07-13 13:53:40 +0530

    product page

--- src/landing_page/NotFound.jsx
+++ src/landing_page/NotFound.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import Navbar from './navbar';
+import { Link } from 'react-router-dom';
+
 function NotFound() {
-    return (  
-        <>
-       <div className='container p-5 mb-5'>
-            <div className="row text-center">
-                <p className='mt-5'>  404  </p>
-                <h1>Kiaan couldn’t find that page</h1>
-                <p>We coouldn't find the page you were looking for.Visit <link to="/"></link></p>
-            </div>
-     
-             
-        </div>
-           </> 
-    );
+  return (
+    <div className='container p-5 mb-5'>
+      <div className="row text-center">
+        <p className='mt-5'>404</p>
+        <h1>Kiaan couldn’t find that page</h1>
+        <p>
+          We couldn't find the page you were looking for. Visit{' '}
+          <Link to="/">home</Link>.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
--- src/landing_page/products/leftimage.jsx
+++ src/landing_page/products/leftimage.jsx
@@ -1,8 +1,43 @@
-import React from 'react';
-function LeftImage() {
-    return ( 
-        <h1>LeftImage</h1>
-     );
+import React from "react";
+function LeftImage({
+  imageURL,
+  productName,
+  productDesription,
+  tryDemo,
+  learnMore,
+  googlePlay,
+  appStore,
+}) {
+  return (
+    <div className="container " style={{marginTop:"150px"}}>
+      <div className="row">
+        <div className="col-6">
+          <img src={imageURL} style={{width:"80%",marginLeft:"25%"}} />
+        </div>
+        <div className="col-6 p-5 mt-5">
+          <h1 style={{fontSize:"20px" ,marginLeft:"20%"}}>{productName}</h1>
+          <p style={{fontSize:"15px" ,marginLeft:"20%",paddingRight:"10px"}}>{productDesription}</p>
+          <div>
+            <a href={tryDemo} style={{fontSize:"20px" ,marginLeft:"20%"}}>Try Demo</a>
+            <a href={learnMore} style={{ marginLeft: "50px" }} style={{fontSize:"20px" ,marginLeft:"20%"}}>
+              Learn More
+            </a>
+          </div>
+          <div className="mt-3">
+            <a href={googlePlay} style={{fontSize:"20px" ,marginLeft:"20%"}}>
+              <img src="media/images/googlePlayBadge.svg" />
+            </a>
+            <a href={appStore} style={{fontSize:"20px" ,marginLeft:"20%"}}  >
+              <img
+                src="media/images/appstoreBadge.svg"
+                style={{ marginLeft: "50px" }}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LeftImage;
